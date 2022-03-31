f18af7c1cf1c8cec6dc0e9be8b8e31d9d1a64a53
Phues <75377054+Phues@users.noreply.github.com>
2022-03-31 14:47:57 +0100

    created java classes for layouts

---
+++ app/src/main/java/dz/esisba/a2cpi_project/SearchActivity.java
@@ -0,0 +1,14 @@
+package dz.esisba.a2cpi_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
