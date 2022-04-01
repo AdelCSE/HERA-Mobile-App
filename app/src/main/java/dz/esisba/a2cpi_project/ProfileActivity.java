d87227d82a96a0cfcce905b06750891caa78b38d
AdelCSE <aa.mokadem@esi-sba.dz>
2022-04-01 02:41:54 +0200

    Profile Scrolling fixed

--- app/src/main/java/dz/esisba/a2cpi_project/ProfileActivity.java
+++ app/src/main/java/dz/esisba/a2cpi_project/ProfileActivity.java
@@ -1,8 +1,17 @@
 package dz.esisba.a2cpi_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.palette.graphics.Palette;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = findViewById(R.id.ToolBar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        final CollapsingToolbarLayout collapsingtoolbarlayout = findViewById(R.id.CollapsingToolBarLayout);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.exemple);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(@Nullable Palette palette) {
+                if (palette != null){
+                    collapsingtoolbarlayout.setContentScrimColor(palette.getMutedColor(androidx.appcompat.R.attr.colorPrimary));
+                }
+            }
+        });
+
     }
 }
