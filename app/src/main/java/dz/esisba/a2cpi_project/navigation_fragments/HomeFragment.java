9eb55240a1c057962957b2a8740e64177aac051e
Phues <75377054+Phues@users.noreply.github.com>
2022-05-03 22:39:17 +0100

    displaying posts fixed bugs

--- app/src/main/java/dz/esisba/a2cpi_project/navigation_fragments/HomeFragment.java
+++ app/src/main/java/dz/esisba/a2cpi_project/navigation_fragments/HomeFragment.java
@@ -17,18 +17,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.common.data.DataHolder;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import dz.esisba.a2cpi_project.AddPostActivity;
 import dz.esisba.a2cpi_project.R;
 import dz.esisba.a2cpi_project.SearchActivity;
 import dz.esisba.a2cpi_project.adapter.PostAdapter;
@@ -42,6 +47,7 @@ public class HomeFragment extends Fragment{
     private FirebaseAuth auth;
     private FirebaseUser user;
     private FirebaseFirestore fstore;
+    private DocumentReference askedByRef;
     private String downloadUrl;
 
     @Nullable
@@ -53,7 +59,6 @@ public class HomeFragment extends Fragment{
         recyclerView = parentHolder.findViewById(R.id.recview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        PostsDataHolder = new ArrayList<>();
 
         /*PostModel Post1 = new PostModel(R.drawable.exemple, "Adel Mokadem" , "@addy1001" , "What's your Question1" , "details here","1000","500","11:11 AM • 29 APR 22");
         PostsDataHolder.add(Post1);
@@ -66,20 +71,43 @@ public class HomeFragment extends Fragment{
         PostModel Post5 = new PostModel(R.drawable.exemple, "Rachid Benayad" , "@rachide" , "What's your Question5" , "details here","1000","500","08:25 AM • 29 APR 22");
         PostsDataHolder.add(Post5);*/
 
+        auth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        user = auth.getCurrentUser();
 
-        FetchPosts();
+        askedByRef = FirebaseFirestore.getInstance().collection("Users").document(user.getUid());
+        //get username of poster
+        askedByRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(this.toString(), "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    if (snapshot.get("profilePictureUrl") != null) {
+                        downloadUrl = snapshot.get("profilePictureUrl").toString();
+                    }
+                    FetchPosts();
+                }
+                else {
+                    Log.d(this.toString(), "Current data: null");
+                }
+            }
+        });
 
 
-       if (PostsDataHolder.size()>0)recyclerView.setAdapter(new PostAdapter(PostsDataHolder));
+
 
 
         return parentHolder;
     }
 
     private void FetchPosts() {
-        auth = FirebaseAuth.getInstance();
-        fstore = FirebaseFirestore.getInstance();
-        user = auth.getCurrentUser();
+
+        PostsDataHolder = new ArrayList<>();
 
         fstore.collection("Posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -87,16 +115,15 @@ public class HomeFragment extends Fragment{
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         PostModel post = document.toObject(PostModel.class);
+                        post.setPublisherPic(downloadUrl);
                         PostsDataHolder.add(post);
+                        recyclerView.setAdapter(new PostAdapter(PostsDataHolder));
                     }
                 } else {
                     Toast.makeText(getActivity(), "Network error", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
-
-
     }
 
 }
