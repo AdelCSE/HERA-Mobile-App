13753f4c4ca9b4904e08538bdd7d4432b74747e6
Phues <75377054+Phues@users.noreply.github.com>
2022-05-05 16:51:38 +0100

    loading pp inside bottom nav

--- app/src/main/java/dz/esisba/a2cpi_project/BottomNavigationActivity.java
+++ app/src/main/java/dz/esisba/a2cpi_project/BottomNavigationActivity.java
@@ -1,16 +1,39 @@
 package dz.esisba.a2cpi_project;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import dz.esisba.a2cpi_project.navigation_fragments.AddPostFragment;
 import dz.esisba.a2cpi_project.navigation_fragments.HomeFragment;
@@ -20,12 +43,23 @@ import dz.esisba.a2cpi_project.navigation_fragments.SmartRoomFragment;
 
 public class BottomNavigationActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private FirebaseFirestore fstore;
+    private DocumentReference askedByRef;
+    private String downloadUrl;
+
+    private ImageView profileImg;
+    private Drawable icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation_layout);
+
+        profileImg = findViewById( R.id.ppimg);
 
         //setEnabled(false) for the 2nd index because we're using a floating button instead
         bottomNav.setBackground(null);
@@ -46,6 +80,53 @@ public class BottomNavigationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        auth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        user = auth.getCurrentUser();
+        DocumentReference df = fstore.collection("Users").document(user.getUid());
+        //grab the existing info
+        df.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists())
+                    {
+                        if (doc.get("profilePictureUrl")!= null) {
+                            downloadUrl = doc.get("profilePictureUrl").toString();
+                            Glide.with(getApplicationContext()).asBitmap().circleCrop().load(downloadUrl)
+                                    .into(new CustomTarget<Bitmap>() {
+
+                                        @Override
+                                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                            bottomNav.setItemIconTintList(null);
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                                bottomNav.getMenu().getItem(4).setIconTintList(null);
+                                            }
+                                            Drawable profileImage = new BitmapDrawable(getResources(), resource);
+                                            bottomNav.getMenu().getItem(4).setIcon(profileImage);
+                                        }
+
+                                        @Override
+                                        public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                        }
+                                    });
+                        }
+                    }
+                }
+            }
+        });
+        bottomNav.getMenu().getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                bottomNav.getMenu().getItem(2).setIcon(R.drawable.ic_arrow);
+                return false;
+            }
+        });
+
     }
 
     //Switch between fragments
