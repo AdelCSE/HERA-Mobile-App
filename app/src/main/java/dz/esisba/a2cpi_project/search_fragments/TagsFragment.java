da6fb76659ec94d5d7f61c776cc296afb6348ae8
AdelCSE <aa.mokadem@esi-sba.dz>
2022-05-12 18:01:01 +0200

    Tags filter

--- app/src/main/java/dz/esisba/a2cpi_project/search_fragments/TagsFragment.java
+++ app/src/main/java/dz/esisba/a2cpi_project/search_fragments/TagsFragment.java
@@ -1,8 +1,12 @@
 package dz.esisba.a2cpi_project.search_fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +14,41 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.chip.Chip;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import dz.esisba.a2cpi_project.QuestionBlocActivity;
 import dz.esisba.a2cpi_project.R;
+import dz.esisba.a2cpi_project.adapter.PostAdapter;
+import dz.esisba.a2cpi_project.adapter.SearchRecommendationAdapter;
 import dz.esisba.a2cpi_project.interfaces.QuestionsOnItemClickListner;
 import dz.esisba.a2cpi_project.interfaces.SearchOnItemClick;
+import dz.esisba.a2cpi_project.models.PostModel;
+import dz.esisba.a2cpi_project.models.UserModel;
 
 public class TagsFragment extends Fragment implements SearchOnItemClick {
 
-    View parentHolder;
+    private View parentHolder;
+    private RecyclerView recyclerView;
+    private ArrayList<PostModel> QuestionsDataHolder;
+    private SearchRecommendationAdapter adapter;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private FirebaseFirestore fstore;
+    private DocumentReference likesRef;
+    private CollectionReference postRef;
+
     Chip all,newborn, kid, baby,
             sleeping,healthcare,breastfeeding,needs,circumcision,routine,food,
             fever, influenza,hepatitis,conjunctivitis,
@@ -30,28 +60,49 @@ public class TagsFragment extends Fragment implements SearchOnItemClick {
                              Bundle savedInstanceState) {
         parentHolder = inflater.inflate(R.layout.fragment_tags, container, false);
 
+        auth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        user = auth.getCurrentUser();
+        postRef = fstore.collection("Posts");
+        QuestionsDataHolder = new ArrayList<>();
+
         InitChips();
         ShowTagResults();
 
         return parentHolder;
     }
 
+    public void BuildRecyclerView(){
+        recyclerView = parentHolder.findViewById(R.id.searchTagsRecview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new SearchRecommendationAdapter(QuestionsDataHolder,this);
+        recyclerView.setAdapter(adapter);
+    }
+
     @Override
     public void onItemClick(int position) {
-
+        PostModel Post1 = new PostModel(QuestionsDataHolder.get(position).getAskedBy(), QuestionsDataHolder.get(position).getPublisher()
+                , QuestionsDataHolder.get(position).getUsername() , QuestionsDataHolder.get(position).getQuestion() ,
+                QuestionsDataHolder.get(position).getBody(),QuestionsDataHolder.get(position).getPostid(),
+                QuestionsDataHolder.get(position).getDate(),QuestionsDataHolder.get(position).getPublisherPic(),
+                QuestionsDataHolder.get(position).getLikesCount(),QuestionsDataHolder.get(position).getAnswersCount(), QuestionsDataHolder.get(position).getTags());
+        Intent intent = new Intent(getActivity(), QuestionBlocActivity.class);
+        intent.putExtra("Tag", (Serializable) Post1);
+        getActivity().startActivity(intent);
     }
 
     @Override
     public void onAnswerClick(int position) {
-
+        PostModel Post1 = new PostModel(QuestionsDataHolder.get(position).getAskedBy(), QuestionsDataHolder.get(position).getPublisher()
+                , QuestionsDataHolder.get(position).getUsername() , QuestionsDataHolder.get(position).getQuestion() ,
+                QuestionsDataHolder.get(position).getBody(),QuestionsDataHolder.get(position).getPostid(),
+                QuestionsDataHolder.get(position).getDate(),QuestionsDataHolder.get(position).getPublisherPic(),
+                QuestionsDataHolder.get(position).getLikesCount(),QuestionsDataHolder.get(position).getAnswersCount(), QuestionsDataHolder.get(position).getTags());
+        Intent intent = new Intent(getActivity(), QuestionBlocActivity.class);
+        intent.putExtra("Tag", (Serializable) Post1);
+        getActivity().startActivity(intent);
     }
 
-    public void GettSelectedTag (){
-
-
-
-
-    }
 
     private void ShowTagResults()
     {
@@ -61,7 +112,28 @@ public class TagsFragment extends Fragment implements SearchOnItemClick {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b){
-                    Toast.makeText(getActivity(), compoundButton.getText().toString(), Toast.LENGTH_SHORT).show();
+                    postRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                QuestionsDataHolder = new ArrayList<>();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    PostModel post = document.toObject(PostModel.class);
+                                    ArrayList<String> tags = post.getTags();
+                                    if (tags!=null){
+                                        for (String tag : tags){
+                                            if (compoundButton.getText().toString().equals(tag)){
+                                                QuestionsDataHolder.add(post);
+                                            }
+                                        }
+                                    }
+                                }
+                                BuildRecyclerView();
+                            } else {
+                                Toast.makeText(getActivity(), "Network error", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
 
                 }else{
 
