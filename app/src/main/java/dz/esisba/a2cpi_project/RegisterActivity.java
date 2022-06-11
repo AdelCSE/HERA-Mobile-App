9632d0d8f14749e2aa0007a05843ca6c3a53646a
Phues <75377054+Phues@users.noreply.github.com>
2022-06-11 23:37:52 +0100

    home

--- app/src/main/java/dz/esisba/a2cpi_project/RegisterActivity.java
+++ app/src/main/java/dz/esisba/a2cpi_project/RegisterActivity.java
@@ -22,6 +22,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -30,6 +32,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import dz.esisba.a2cpi_project.models.PostModel;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -146,6 +150,17 @@ public class RegisterActivity extends AppCompatActivity {
                                 userInfor.put("isAdmin",false);
 
                                 df.set(userInfor); //pass our map to the fb document
+                                fstore.collection("Posts").orderBy("likesCount", Query.Direction.DESCENDING).limit(50)
+                                        .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                            PostModel post = document.toObject(PostModel.class);
+                                            df.collection("Feed").document(post.getPostid()).set(post);
+                                            df.collection("Feed").document(post.getPostid()).update("priority", 2);
+                                        }
+                                    }
+                                });
 
                                 user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
