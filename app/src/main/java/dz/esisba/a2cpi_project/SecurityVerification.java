cd470a34909992d30a8f47102dc9a9d7e257624d
zineddine <mr.benayad@esi-sba.dz>
2022-06-11 16:50:52 +0100

    Security

--- app/src/main/java/dz/esisba/a2cpi_project/SecurityVerification.java
+++ app/src/main/java/dz/esisba/a2cpi_project/SecurityVerification.java
@@ -1,14 +1,13 @@
 package dz.esisba.a2cpi_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,7 +20,6 @@ import java.util.Map;
 
 public class SecurityVerification extends AppCompatActivity {
     private EditText verificationCode;
-    private Button verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +27,25 @@ public class SecurityVerification extends AppCompatActivity {
         setContentView(R.layout.activity_security_verification);
 
         verificationCode = findViewById(R.id.verificationCode);
-        verify = findViewById(R.id.Verifybtn);
+        Button verify = findViewById(R.id.Verifybtn);
 
-        verify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String code = verificationCode.getText().toString();
-                Bundle extras = getIntent().getExtras();
-                if (extras != null) {
-                    String value = extras.getString("verificationCode");
-                    if(value.equals(code)){
-                        //open home
-                        //login directly
-                        retrieveRestoreToken();
-                        startActivity(new Intent(SecurityVerification.this, BottomNavigationActivity.class));
-                        finish();
-                    }else{
-                        Toast.makeText(SecurityVerification.this, "Incorrect Code , Try Again", Toast.LENGTH_SHORT).show();
-                    }
+        verify.setOnClickListener(v -> {
+            String code = verificationCode.getText().toString();
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                String value = extras.getString("verificationCode");
+                if(value.equals(code)){
+                    //open home
+                    //login directly
+                    retrieveRestoreToken();
+                    Toast.makeText(SecurityVerification.this, "Log-in Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SecurityVerification.this, BottomNavigationActivity.class));
+                    finish();
                 }else{
-                    Toast.makeText(SecurityVerification.this, "Email was not sent ,Try Again", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SecurityVerification.this, "Incorrect Code , Try Again", Toast.LENGTH_SHORT).show();
                 }
+            }else{
+                Toast.makeText(SecurityVerification.this, "Email was not sent ,Try Again", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -63,7 +59,7 @@ public class SecurityVerification extends AppCompatActivity {
                     String token = task.getResult();
                     userToken.put("Token", token);
                     FirebaseFirestore.getInstance().collection("Users").document(
-                            FirebaseAuth.getInstance().getCurrentUser().getUid()).update(userToken).addOnSuccessListener(unused -> Toast.makeText(SecurityVerification.this,"Succccessss",Toast.LENGTH_LONG));
+                            FirebaseAuth.getInstance().getCurrentUser().getUid()).update(userToken).addOnSuccessListener(unused -> Toast.makeText(SecurityVerification.this,"Success",Toast.LENGTH_LONG));
                 }
             }
         });
